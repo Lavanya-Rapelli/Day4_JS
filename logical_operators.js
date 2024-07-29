01b1d2d2caa34eac72c0941d8207bd9b5bf14206
Lavanya Rapelli <lavanyarapelli555@gmail.com>
2024-07-29 18:17:37 +0530

    conditional statements and logical operators

---
+++ logical_operators.js
@@ -0,0 +1,55 @@
+
+
+// console.log(true && true); //true
+// console.log(false && true); //false
+
+// let palakAvailable = true;
+// let paneerAvailable = true;
+
+// if (palakAvailable && paneerAvailable) {
+//     console.log("We have palak paneer")
+// }
+// else{
+//     console.log("we dont have food today")
+// }
+
+// let gender = "male"
+// let age = 19;
+
+// if(gender == "male" && age >= 21){
+//    console.log("you can marry")
+// }
+// else if(gender == "female" && age >=18){
+//     console.log("you can marry")
+// }
+// else {
+//     console.log("you can not marry ")
+// }
+
+// let num1 = 100;
+// let num2 = 50;
+// let num3 = 20;
+
+// if (num1 > num2 && num1 > num3){
+//     console.log(num1, "is greater")
+// }
+// else if (num2 > num1 && num2 > num3){
+//     console.log(num2, "is greater")
+// }
+// else{
+//     console.log(num3, "is greater")
+// }
+
+// Dinner - potato (chilli potato) . paner (chilli paneer )
+
+// let potatoPrsent = false;
+// let paneerPresent = false;
+
+// if (paneerPresent || potatoPrsent ){
+//     console.log("yes you will have dinner today")
+// }
+// else{
+//     console.log("Nothing for today again a fast day")
+// }
+
+
---
+++ statements.js
@@ -0,0 +1,94 @@
+// if else else if conditional statements
+
+// let num = 10;
+
+// if (num > 5) {
+//     console.log('The number is greater than 5.');
+// }
+
+// let num = 3;
+
+// if (num > 5) {
+//     console.log('The number is greater than 5.');
+// } else {
+//     console.log('The number is not greater than 5.');
+// }
+
+// let num = 10;
+
+// if (num > 10) {
+//     console.log('The number is greater than 10.');
+// } else if (num === 10) {
+//     console.log('The number is exactly 10.');
+// } else {
+//     console.log('The number is less than 10.');
+// }
+
+// let color = "red";
+
+// if(color === "red") {
+//     console.log("stop the car")
+// }
+// else if (color === "yellow"){
+//     console.log("slow down")
+// }
+// else if(color === "green"){
+//     console.log("go fast")
+// }
+// else{
+//     console.log("Go like you want")
+// }
+
+// let lavviScore = 40;
+
+// if(lavviScore >= 90){
+//     console.log("AA")
+// }
+// else if(lavviScore >= 80){
+//     console.log("A+")
+// }
+// else if (lavviScore >= 70){
+//     console.log("A")
+// }
+// else if (lavviScore >= 60){
+//     console.log("BB")
+// }
+// else{
+//     console.log("lavvi is not a good student")
+// }
+
+// let bill = 15000;
+
+// if(bill >= 5000){
+//     console.log("you got 10% discount")
+// }
+// else if(bill >= 15000){
+//     console.log("you got 20% discount")
+
+// }
+// else if(bill >= 30000){
+//     console.log("you got 30% discount")
+    
+// }
+// else if(bill >= 50000){
+//     console.log("you got 35% discount")
+// }
+// else{
+//     console.log("No discount")
+// }
+
+// let storedusername = "lavanya"
+// let storedpassword ="lavanya@123"
+
+// let inputusername = "lavanya"
+// let inputpassword = "lavanya@123"
+
+// if(storedusername == inputusername){
+//     if(storedpassword == inputpassword){
+//         console.log("you can login")
+//     }
+//     else{
+//     console.log("incorrect details")
+// }
+// }
+
